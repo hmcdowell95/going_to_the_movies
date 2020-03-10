31e0983b1a983e15e1258e9cc13c7c3187518f3c
Harley McDowell <hmcdowell95@gmail.com>
2020-03-10 16:51:39 +0000

    first victory

--- lib/going_to_the_movies/scraper.rb
+++ lib/going_to_the_movies/scraper.rb
@@ -4,19 +4,29 @@ require 'open-uri'
 class Scraper
   
   def get_page
-    Nokogiri::HTML(open("https://www.amctheatres.com/"))
+    Nokogiri::HTML(open("https://www.imdb.com/movies-in-theaters/"))
   end
   
   def self.movie_list
-    url = Nokogiri::HTML(open("https://www.amctheatres.com/"))
-    a = url.css("body").css("div").css("div.poster-carousel-wrapper.MoviePosters.is-standalone").css("div").css("div").css("div").css("div")
-    b = []
-    binding.pry
-    a.each do |x|
-      b << x.css("h3").text
+    url = Nokogiri::HTML(open("https://www.imdb.com/movies-in-theaters/"))
+    a = url.css("body").css("div#wrapper").css("div#root").css("div#pagecontent").css("div#content-2-wide").css("div#main").css("div").css("div")
+    b = a.css("h4").text
+    c = b.split(")")
+    c.each do |x|
+      x[-6..-1] = ""
+      x[0] = ""
     end
-    b
+    c
   end
-
+  
+  def self.movie_span
+    url = Nokogiri::HTML(open("https://www.imdb.com/movies-in-theaters/"))
+    a = url.css("body").css("div#wrapper").css("div#root").css("div#pagecontent").css("div#content-2-wide").css("div#main").css("div").css("div")
+    b = a.css("p").css("time").text
+    c = b.split(" min")
+    c 
+  end
+  
 end
 Scraper.movie_list
+
